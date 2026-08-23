9faf3b316a63d6e8758c8d87759ef1225940cb2a
FerdosFijer <ffijer@gmail.com>
2026-08-23 23:43:45 +0600

    jwt authintication

--- src/components/jobs/JobListingContainer.jsx
+++ src/components/jobs/JobListingContainer.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useMemo } from 'react';
+import { useState, useMemo, useEffect } from 'react';
 import JobCard from "@/components/jobs/JobCard";
 import JobFilter from './JobsFilter';
 
@@ -11,32 +11,39 @@ export default function JobListingContainer({ jobs = [] }) {
   const [typeFilter, setTypeFilter] = useState('all');
   const [remoteFilter, setRemoteFilter] = useState('all');
 
-  const filteredJobs = useMemo(() => {
-    return jobs.filter((job) => {
-      // Search matching (Title, Company Name, Requirements)
-      const matchesSearch =
-        !searchQuery ||
-        job?.title?.toLowerCase().includes(searchQuery.toLowerCase()) ||
-        job?.companyName?.toLowerCase().includes(searchQuery.toLowerCase()) ||
-        job?.requirements?.toLowerCase().includes(searchQuery.toLowerCase());
+  useEffect(()=>{
+    const searchParam = new URLSearchParams()
+  },[])
 
-      // Category matching
-      const matchesCategory =
-        categoryFilter === 'all' || job?.category === categoryFilter;
 
-      // Type matching
-      const matchesType =
-        typeFilter === 'all' || job?.type === typeFilter;
 
-      // Remote / Workplace matching
-      const matchesRemote =
-        remoteFilter === 'all' ||
-        (remoteFilter === 'remote' && job?.isRemote) ||
-        (remoteFilter === 'onsite' && !job?.isRemote);
 
-      return matchesSearch && matchesCategory && matchesType && matchesRemote;
-    });
-  }, [jobs, searchQuery, categoryFilter, typeFilter, remoteFilter]);
+  // const jobs = useMemo(() => {
+  //   return jobs.filter((job) => {
+  //     // Search matching (Title, Company Name, Requirements)
+  //     const matchesSearch =
+  //       !searchQuery ||
+  //       job?.title?.toLowerCase().includes(searchQuery.toLowerCase()) ||
+  //       job?.companyName?.toLowerCase().includes(searchQuery.toLowerCase()) ||
+  //       job?.requirements?.toLowerCase().includes(searchQuery.toLowerCase());
+
+  //     // Category matching
+  //     const matchesCategory =
+  //       categoryFilter === 'all' || job?.category === categoryFilter;
+
+  //     // Type matching
+  //     const matchesType =
+  //       typeFilter === 'all' || job?.type === typeFilter;
+
+  //     // Remote / Workplace matching
+  //     const matchesRemote =
+  //       remoteFilter === 'all' ||
+  //       (remoteFilter === 'remote' && job?.isRemote) ||
+  //       (remoteFilter === 'onsite' && !job?.isRemote);
+
+  //     return matchesSearch && matchesCategory && matchesType && matchesRemote;
+  //   });
+  // }, [jobs, searchQuery, categoryFilter, typeFilter, remoteFilter]);
 
   return (
     <div className="space-y-6">
@@ -54,8 +61,8 @@ export default function JobListingContainer({ jobs = [] }) {
 
       {/* 3-Column Responsive Grid */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center">
-        {filteredJobs.length > 0 ? (
-          filteredJobs.map((job) => (
+        {jobs.length > 0 ? (
+          jobs.map((job) => (
             <JobCard key={job?._id || job?.title} job={job} />
           ))
         ) : (
--- src/lib/core/server.js
+++ src/lib/core/server.js
@@ -1,17 +1,42 @@
+import { redirect } from "next/navigation";
+import { getUserToken } from "./session";
+
 const baseURL = process.env.NEXT_PUBLIC_BASE_URL
 
+export const authHeader = async() =>{
+    const token = await getUserToken();
+    const header = token ? {
+        authorization: `bearer ${token}`
+    } : {};
+    return header;
+}
 export const serverFetch = async(path)=> {
     const res = await fetch(`${baseURL}${path}`);
-    return res.json();
+    return handleStatusCode(res);
 }
 
-
+export const protectedFetch = async (path)=> {
+    const res = await fetch(`${baseURL}${path}`,
+        {headers:await authHeader()}
+    );
+    return handleStatusCode(res);
+}
 
 export const serverMutation =async (path, data,  method='POST') =>{
     const res = await fetch (`${baseURL}${path}`,{
         method: method,
-        headers:{'content-type': 'application/json'},
+        headers:{'content-type': 'application/json', ...await authHeader()},
         body: JSON.stringify(data)
     })
-    return res.json();
+    return handleStatusCode(res);
+}
+
+const handleStatusCode = res => {
+    if(res.status === 401){
+        redirect('/unauthorized')
+    }
+    else if(res.status === 403) {
+        redirect('/unauthorized');
+    }
+    return res.json()
 }
